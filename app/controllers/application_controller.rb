1b9ef4fba2003bf868aa5fa60f0474d37e8c01ec
TarrinRos <tarindis@gmail.com>
2018-12-20 19:44:55 +0200

    Alpha version ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :current_user_can_edit?
+
   def configure_permitted_parameters
     # .for method was changed by the .permit method for devise version >= 4
     devise_parameter_sanitizer.permit(:account_update) { |u|
       u.permit(:password, :password_confirmation, :current_password)
     }
+  end
+
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def user_avatar(user)
+  #   TODO: real user avatars
+    asset_path 'user.png'
+  end
 end
