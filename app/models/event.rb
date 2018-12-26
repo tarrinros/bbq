2f6c0ed36ba51a20f63c017004baa7fc4dd40006
TarrinRos <tarindis@gmail.com>
2018-12-26 23:54:17 +0200

    Add photos controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   has_many :comments
   has_many :subscriptions
   has_many :subscribers, through: :subscriptions, source: :user
+  has_many :photos
 
   validates :user, presence: true
   validates :title, presence: true, length: {maximum: 255}
