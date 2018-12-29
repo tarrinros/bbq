047b0a9b9d4aaf4ee0026bdf213352edb3867129
TarrinRos <tarindis@gmail.com>
2018-12-29 20:06:31 +0200

    Add validation for anonyms emal presence in DB

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,12 +2,20 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
+  # Validates users email by presence in DB when anonym tries to subscribe for event
+  validate :email_in_db_presence, unless: -> { user.present? }
+
   validates :event, presence: true
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, email: true, unless: -> { user.present? }
 
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+
+
+  def email_in_db_presence
+    errors.add(:user_email, :email_presence) if User.exists?(email: self.user_email)
+  end
 
   def user_name
     if user.present?
