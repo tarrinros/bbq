b72d31b29257185c4c6b715f01cfe6c372f9607e
TarrinRos <tarindis@gmail.com>
2018-12-30 18:21:58 +0200

    Change notify subscribers method in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,6 +44,10 @@ class CommentsController < ApplicationController
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
+
+    # Deletes author of the comment from array
+    all_emails.delete_if { |email| email == current_user.email }
+
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке
