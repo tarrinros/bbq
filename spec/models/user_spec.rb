cf3e7bdf09e6e59b28cfaccdc6aa85b62561dd7b
tarrinros <tarindis@gmail.com>
2019-08-26 01:32:57 +0300

    Add test for invalid User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,15 +5,40 @@ RSpec.describe User, type: :model do
 
   it "is valid with a name, email, and password" do
     user = User.new(
-      name: "Aaron",
+      name: "Vanya",
       email: "tester@example.com",
       password: "dottle-nouveau-pavilion-tights-furze"
     )
     expect(user).to be_valid
   end
 
-  it "is invalid without a name"
-  it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
+  # it "is invalid without a name" do
+  #   user = User.new(name: nil)
+  #   user.valid?
+  #   expect(user.errors[:name]).to include("не может быть пустым")
+  # end
+
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("не может быть пустым")
+  end
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+      name: "Joe",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+      )
+      
+      user = User.new(
+      name: "Jane",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+      )
+      
+      user.valid?
+      expect(user.errors[:email]).to include("уже существует")
+  end
   it "returns a user's full name as a string"
 end
