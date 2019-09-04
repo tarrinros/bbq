232a48ab02c0b1a3ed4630089c7576f88fd7664b
tarrinros <tarindis@gmail.com>
2019-09-04 18:43:57 +0300

    Add spec for users name generator on create

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,12 @@ FactoryBot.define do
   factory :user do
     name {"Alex_#{rand(999)}"}
 
-    sequence(:email) { 'tester@example.com' }
+    sequence(:email) { |n| "tester#{n}@example.com" }
 
     after(:build) { |u| u.password_confirmation = u.password = 'dottle-nouveau-pavilion-tights-furze'}
+
+    # trait :with_generated_name do
+    #   before(:validation) { |project| create_list(:note, 5, project: project) }
+    # end
   end
 end
