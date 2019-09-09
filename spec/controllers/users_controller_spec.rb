81df44f79f3d0303ad770d2245be85673cc6284a
tarrinros <tarindis@gmail.com>
2019-09-09 23:12:33 +0300

    Add spec for edit action in users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to have_http_status '200'
     end
   end
+
+  describe '#edit' do
+    context 'as an authorized user' do
+      before(:each) { sign_in user }
+
+      it 'responds successfully' do
+        get :edit, params: { id: user.id }
+        expect(response).to be_successful
+      end
+
+      it 'returns a 200 response' do
+        get :edit, params: { id: user.id }
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context 'as guest' do
+      it 'returns a 302 response' do
+        post :edit, params: {id: user.id}
+        expect(response).to have_http_status '302'
+      end
+
+      it 'redirects to the sign-in page' do
+        post :edit, params: {id: user.id}
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
 end
