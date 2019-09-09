6ffaf916bb5c13cea1ae78d781023f263cd623af
tarrinros <tarindis@gmail.com>
2019-09-09 17:02:42 +0300

    Modify spec for destroy action of events controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -97,12 +97,11 @@ RSpec.describe EventsController, type: :controller do
   describe '#destroy' do
     context 'as logged in user' do
       it 'deletes the event' do
-        event_params = FactoryBot.attributes_for(:event)
+        event = FactoryBot.create(:event, user: user)
         sign_in user
         expect {
-          delete :destroy, params: {event: event_params}
+          delete :destroy, params: {id: event.id}
         }.to change(user.events, :count).by(-1)
-        expect(response).to be_successful
         expect(response).to have_http_status(:redirect)
       end
     end
